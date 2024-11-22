3c91b7d7798b7c42d9d33aa431030d733ad25c72
JeremyReynolds <Jer24rey@gmail.com>
2024-11-22 12:24:30 -0600

    add all components and assets, no styles, add styles in next commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__content_name">Developed by Jeremy Reynolds</p>
+        <p className="footer__content_year">2024</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,31 @@
+import "./ModalWithForm.css";
+
+function ModalWithForm({
+  children,
+  buttonText,
+  titleText,
+  closeActiveModal,
+  isOpen,
+  handleOverlay,
+}) {
+  return (
+    <div onClick={handleOverlay} className={`modal ${isOpen && "modal_open"}`}>
+      <div className="modal__content">
+        <h2 className="modal__title">{titleText}</h2>
+        <button
+          onClick={closeActiveModal}
+          type="button"
+          className="modal__close"
+        />
+        <form className="modal__form">
+          {children}
+          <button className="modal__submit" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
